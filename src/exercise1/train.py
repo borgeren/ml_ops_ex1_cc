2b506796bff63e18e31a4b926a1c4d8d808385ee
borgeren <jeppe-knudsen@hotmail.com>
2026-01-06 12:19:11 +0100

    moved from api to train

--- src/exercise1/train.py
+++ src/exercise1/train.py
@@ -1,10 +1,53 @@
-from exercise1.model import Model
-from exercise1.data import MyDataset
+from exercise1.model import MyAwesomeModel
+from exercise1.data import corrupt_mnist
+import torch
+import matplotlib.pyplot as plt
 
-def train():
-    dataset = MyDataset("data/raw")
-    model = Model()
-    # add rest of your training code here
+
+DEVICE = torch.device("cuda" if torch.cuda.is_available() else "mps" if torch.backends.mps.is_available() else "cpu")
+
+
+def train(lr: float = 1e-3, batch_size: int = 32, epochs: int = 10, save_model: str = "./models/model.pth", plot: bool = False) -> None:
+    """Train a model on MNIST."""
+    print("Training day and night")
+    print(f"{lr=}, {batch_size=}, {epochs=}")
+
+    model = MyAwesomeModel().to(DEVICE)
+    train_set, _ = corrupt_mnist()
+
+    train_dataloader = torch.utils.data.DataLoader(train_set, batch_size=batch_size)
+
+    loss_fn = torch.nn.CrossEntropyLoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+    statistics = {"train_loss": [], "train_accuracy": []}
+    for epoch in range(epochs):
+        model.train()
+        for i, (img, target) in enumerate(train_dataloader):
+            img, target = img.to(DEVICE), target.to(DEVICE)
+            optimizer.zero_grad()
+            y_pred = model(img)
+            loss = loss_fn(y_pred, target)
+            loss.backward()
+            optimizer.step()
+            statistics["train_loss"].append(loss.item())
+
+            accuracy = (y_pred.argmax(dim=1) == target).float().mean().item()
+            statistics["train_accuracy"].append(accuracy)
+
+            if i % 100 == 0:
+                print(f"Epoch {epoch}, iter {i}, loss: {loss.item()}")
+
+    print("Training complete")
+    if save_model:
+        torch.save(model.state_dict(), save_model)
+    if plot:
+        fig, axs = plt.subplots(1, 2, figsize=(15, 5))
+        axs[0].plot(statistics["train_loss"])
+        axs[0].set_title("Train loss")
+        axs[1].plot(statistics["train_accuracy"])
+        axs[1].set_title("Train accuracy")
+        fig.savefig("/reports/figures/training_statistics.png")
 
 if __name__ == "__main__":
     train()
